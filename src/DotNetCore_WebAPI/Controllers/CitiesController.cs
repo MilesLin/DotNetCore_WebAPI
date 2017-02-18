dc19dd786e2db5cbe6adc8e3376a05eddd760382
Miles <miles@miniasp.com>
2017-02-18 11:10:40 +0800

    Add CitiesController

---
+++ src/DotNetCore_WebAPI/Controllers/CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DotNetCore_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class CitiesController : Controller
+    {
+        [HttpGet]
+        public ActionResult GetCities()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new { id = 1, Name = "New York City"},
+                new { id = 2, Name = "Antwerp"},
+            });
+        }
+    }
+}
